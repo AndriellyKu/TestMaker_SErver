a0d6a7c5da538f0403cf137fdcba9a00ebce55e9
Andrielly Kotcheski <Andrellykotcheskio362@gmail.com>
2024-11-17 15:07:18 -0300

    inicio da prova criando

--- src/routes/provaRoutes.js
+++ src/routes/provaRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { listarProvasDoProfessor, excluirProva, criarProva } = require('../controllers/provaController');
+const { gerarQuestoes } = require('../controllers/openAIController');  // Importando a função de gerar questões
 const authMiddleware = require('../middleware/authenticateToken');
 
 // Rota para listar as provas do professor
@@ -11,5 +12,8 @@ router.post('/criar-prova', authMiddleware, criarProva);
 
 // Rota para excluir uma prova (usando o ID da prova)
 router.delete('/excluir-prova/:id', excluirProva);
+
+// Rota para gerar questões para a prova
+router.post('/gerar-questoes', authMiddleware, gerarQuestoes);  // Adicionando a rota para gerar questões
 
 module.exports = router;
