25fd7b61dd3e6e6f763f65c4973b25d450f0de8b
Andrielly Kotcheski <Andrellykotcheskio362@gmail.com>
2024-11-17 15:54:43 -0300

    socorro

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Alterado para bcryptjs
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -37,25 +37,25 @@ const userSchema = new mongoose.Schema({
     }]
 });
 
+// Middleware para hash de senha antes de salvar o usuário
 userSchema.pre('save', async function (next) {
- 
     if (!this.isModified('password')) return next();
 
     try {
-        const salt = await bcrypt.genSalt(10); 
-        this.password = await bcrypt.hash(this.password, salt); 
-        next(); 
+        const salt = await bcrypt.genSalt(10); // Gera o salt
+        this.password = await bcrypt.hash(this.password, salt); // Hasheia a senha
+        next(); // Continua o fluxo
     } catch (error) {
-        next(error); 
+        next(error); // Passa o erro para o próximo middleware
     }
 });
 
-
+// Método para comparar senhas
 userSchema.methods.comparePassword = async function (password) {
-    return bcrypt.compare(password, this.password);
+    return bcrypt.compare(password, this.password); // Compara a senha fornecida com a hasheada
 };
 
-
+// Criação do modelo User
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
